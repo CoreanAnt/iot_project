e4573577556f32b833aeb1fc06c6404b674d9347
CoreanAnt <dlxogus96@naver.com>
2023-03-29 14:20:33 +0900

    ver0.5

---
+++ step+IR/step+IR.h
@@ -0,0 +1,12 @@
+﻿#ifndef STEP_H_
+#define STEP_H_
+
+void init();
+void step_forward();
+void step_backward();
+uint8_t read_ir();
+
+
+
+
+#endif /* STEP_H_ */
--- uart/uart.h
+++ uart/uart.h
@@ -1,9 +1,6 @@
 ﻿#ifndef UART_H_
 #define UART_H_
 
-char p[12];
-
-
 void USART_Init(void);
 unsigned char USART_Receive(void);
 void USART_Transmit(unsigned char data);
@@ -16,8 +13,5 @@ void UART_printString(char *str);
 void UART_print8bitNumber(uint8_t no);
 void UART_print16bitNumber(uint16_t no);
 void UART_print32bitNumber(uint32_t no);
-void PutChar0(char c);
-void Puts0(char* str);
-char Gets0(void);
 
 #endif /* UART_H_ */
